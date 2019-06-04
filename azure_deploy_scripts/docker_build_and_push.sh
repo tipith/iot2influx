f4ec5184410f495713bb45d281113fab783e3639
Timo Pihlström <timo.pihlstrom@iki.fi>
2019-06-04 09:17:50 +0300

    docker build and push script

--- azure_deploy_scripts/docker_build_and_push.sh
+++ azure_deploy_scripts/docker_build_and_push.sh
@@ -1,10 +1,10 @@
-# must first login with docker login --username=yourhubusername --email=youremail@company.com
-my_dir="$(dirname "$0")"
+# must first login with: docker login --username=yourhubusername --email=youremail@company.com
+# optionally run with: docker run --name iot2influx --net container:influxdb -v $(pwd)/config.ini:/opt/iot/config.ini iot2influx 
 
+my_dir="$(dirname "$0")"
 source $my_dir/common.sh
 
 tag=${docker_user}/${docker_img_name}
 docker build -t ${tag}:latest $my_dir/..
 docker push ${tag}
 
-# optionally run with
